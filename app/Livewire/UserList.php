b0bb491f86159fab71bae38f40e5034a961a5634
Murillo-ES <murillo.euflausino@gmail.com>
2025-02-12 16:09:21 -0300

    User search functionality added.

--- app/Livewire/UserList.php
+++ app/Livewire/UserList.php
@@ -10,9 +10,24 @@ class UserList extends Component
 {
     use WithPagination;
 
+    public $searchInput = '';
+    public $error = '';
+
+    // Reset pagination when search input is updated
+    public function updatedSearchInput()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        $users = User::paginate(10);
+        $query = User::query();
+
+        if (!empty($this->searchInput)) {
+            $query->where('name', 'like', '%' . $this->searchInput . '%');
+        }
+
+        $users = $query->paginate(10);
 
         return view('livewire.user-list', compact('users'));
     }
--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -1,27 +1,48 @@
 <div>
-    <table class="min-w-full table-auto border-collapse border border-gray-300 dark:border-gray-600 highlight centered">
-        <thead>
-            <tr>
-                <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Nome de Usuário</th>
-                <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Data de Cadastro</th>
-                <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Quantidade de Produtos</th>
-                <th class="border border-bottom-gray-300 dark:border-gray-600 px-4 py-2"></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($users as $user)
-                <tr wire:key="{{ $user->id }}">
-                    <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{ $user->name }}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{$user->created_at->format('d/m/Y')}}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{count($user->products)}}</td>
-                    <td class="border border-gray-300 dark:border-gray-600 px-4 py-2">
-                        <a href="{{route('user.details', $user->id)}}" class="waves-effect waves-light btn-small blue darken-4">
-                            Visualizar Usuário<i class="material-icons right">info</i></a>
-                    </td>              
+    <div class="row">
+        <form class="col s12">
+            <div class="input-field col s12">
+                <input placeholder="Busque um usuário!" id="searchInput" type="text" wire:model.live="searchInput" class="center-align">
+                <label class="label-icon right" for="searchInput"><i class="material-icons">search</i></label>
+            </div>
+        </form>
+    </div>
+
+    @if ($users->isEmpty())
+        <table class="min-w-full table-auto border-collapse border border-gray-300 dark:border-gray-600 highlight centered">
+            <thead>
+                <tr>
+                    <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Nenhum usuário localizado!</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    @else
+        <table class="min-w-full table-auto border-collapse border border-gray-300 dark:border-gray-600 highlight centered">
+            <thead>
+                <tr>
+                    <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Nome de Usuário</th>
+                    <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Data de Cadastro</th>
+                    <th class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">Quantidade de Produtos</th>
+                    <th class="border border-bottom-gray-300 dark:border-gray-600 px-4 py-2"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                    <tr wire:key="{{ $user->id }}">
+                        <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{ $user->name }}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{$user->created_at->format('d/m/Y')}}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 px-4 py-2 text-black">{{count($user->products)}}</td>
+                        <td class="border border-gray-300 dark:border-gray-600 px-4 py-2">
+                            <a href="{{route('user.details', $user->id)}}" class="waves-effect waves-light btn-small blue darken-4">
+                                Visualizar Usuário<i class="material-icons right">info</i></a>
+                        </td>              
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
     <br>
 
